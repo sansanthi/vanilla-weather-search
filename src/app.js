429e31d17e7c6c8f1a907d30991c9c92cdb3dcc4
San Thi <sansanthi1996@gmail.com>
2023-03-25 02:45:44 +0630

    update real time of city

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  console.log(timestamp);
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${days[day]} ${hours}:${minutes}`;
+}
+
 let apiKey = "6abba6e892082f6e99ff3292ea97c423";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
@@ -5,12 +29,14 @@ function displayTemperature(response) {
   console.log(response);
   let city = document.querySelector("#city");
   let description = document.querySelector(".description");
+  let dateElement = document.querySelector("#date");
   let temperature = document.querySelector("#temperature");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
 
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].main;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   temperature.innerHTML = Math.round(response.data.main.temp);
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = response.data.wind.speed;
